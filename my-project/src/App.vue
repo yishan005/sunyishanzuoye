8fe1ea0d367ca00d87245b6fb1140cac57666fd5
sunyishan <yishan_0801@163.com>
2023-09-19 23:16:08 +0800

    初始化代码

---
+++ my-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>姓名：孙镱珊</h1>
+  <h1>学号：2310274001</h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
